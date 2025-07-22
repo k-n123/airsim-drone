eb5cec74f2e8c8584f1c339d4d2450b362b48ff6
Krish Nachnani <krishnachnani@icloud.com>
2025-07-22 17:35:45 -0500

    Threading

--- drone_extractImages.py
+++ drone_extractImages.py
@@ -1,41 +1,42 @@
-
 from drone_commands import Drone
+
 drone = Drone("192.168.86.48")
 
 import time
+import os
 import threading
-lock = threading.Lock()
-
 import datetime
 
 
-# Drone takeoff = initate client
-drone.takeoff()
+stop_event = threading.Event()
+
 
 # Image capture function (running in background)
 def capture_loop():
-    savedir = "/home/krishnachnani/Desktop/AirsimImages/"
-    date = datetime.datetime.now()
-    savedir = savedir + str(date)
+    savedir = os.path.expanduser("~/Desktop/AirsimImages/")
+    date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    savedir = os.path.join(savedir, date)
     print("Started image capture loop.")
-    while not drone.isLanded():
+    while not stop_event.is_set():
         drone.captureImage(save_dir=savedir)
         time.sleep(0.5)
     print("Stopped image capture loop (drone landed).")
 
+
+# Drone takeoff = initate client
+drone.takeoff()
 
 # Start capture loop in background
 capture_thread = threading.Thread(target=capture_loop)
 capture_thread.start()
 
 # Main thread continues to run commands. Put drone commands here.
-
-with lock:
+try:
     drone.moveTo(20, 20, -20, 3)
     drone.moveTo(-5, -5, -10, 3)
-
-drone.reset()
-
-# Wait for drone to land manually or in another command
-print("Main thread done. Waiting for capture thread to finish if needed...")
-capture_thread.join()
+    input("Press Enter to stop capturing images and land the drone...")
+finally:
+    stop_event.set()
+    capture_thread.join()
+    drone.land()
+    print("Done.")
