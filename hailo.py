edb52532fad10fd0175ddbf2909e03ee2f3db7bd
Krish Nachnani <krishnachnani@icloud.com>
2025-07-29 15:50:41 -0500

    Hailo

---
+++ hailo.py
@@ -0,0 +1,9 @@
+from hailo_platform import VDevice
+from hailo_platform import HEF
+from hailo_platform import InferVStreams
+import cv2
+import numpy as np
+
+# 1. Load your model HEF
+hef_path = "yolov5m_wo_spp.hef"
+hef = HEF(hef_path)
